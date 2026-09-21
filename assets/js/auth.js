0dc3e77bf1dd55ad671c641ddaba6a25e3dafeba
thienngan3004 <thienngan9182@gmail.com>
2026-09-21 14:00:33 +0700

    Update user avatar URL in auth.js for improved image quality

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -20,7 +20,7 @@ const ACCOUNTS = {
     password: '1234',
     name: 'Hồ Thiên Ngân',
     role: 'user',
-    avatar: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=150&auto=format&fit=crop&q=80',
+    avatar: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     email: 'thienngan@gmail.com'
   }
 };
